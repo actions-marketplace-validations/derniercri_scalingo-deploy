be856cacbf9482140bac7017caafda6796bd50e0
Sofiane Gargouri <sofiane@derniercri.io>
2023-11-06 18:29:49 +0100

    :bookmark: 0.0.18

--- src/index.js
+++ src/index.js
@@ -2,47 +2,29 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { createDeployment, getBearerToken } = require('./api');
 
-const formattedRef = () => {
-  const { ref, eventName } = github.context;
-
-  if (eventName === 'pull_request') {
-    return process.env.GITHUB_HEAD_REF;
-  }
-
-  if (ref.startsWith('refs/heads/')) {
-    return ref.replace('refs/heads/', '');
-  }
-  if (ref.startsWith('refs/tags/')) {
-    return ref.replace('refs/tags/', '');
-  }
-
-  return ref;
-};
-
 const getSourceUrl = () => {
   const {
-    ref,
-    eventName,
+    sha,
     serverUrl,
     repo,
   } = github.context;
 
-  const requiredRef = eventName === 'pull_request' ? `refs/heads/${process.env.GITHUB_HEAD_REF}` : ref;
-
-  return `${serverUrl}/${repo.owner}/${repo.repo}/archive/${requiredRef}.tar.gz`;
+  return `${serverUrl}/${repo.owner}/${repo.repo}/archive/${sha}.tar.gz`;
 };
 
 const main = async () => {
   try {
+    const {
+      sha,
+    } = github.context;
     const { token } = await getBearerToken();
-    const ref = formattedRef();
     const sourceUrl = getSourceUrl();
 
     core.debug(`Bearer token: ${token}`);
-    core.info(`Ref: ${ref}`);
+    core.info(`Sha: ${sha}`);
     core.info(`Source URL: ${sourceUrl}`);
 
-    await createDeployment(token, ref, sourceUrl);
+    await createDeployment(token, sha, sourceUrl);
   } catch (err) {
     core.error(err);
   }
