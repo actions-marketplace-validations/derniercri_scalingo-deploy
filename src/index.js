51c2a7cd72d65813d95bb6144630306d10d7f46b
Sofiane Gargouri <sofiane@derniercri.io>
2023-11-06 18:07:40 +0100

    :bookmark: 0.0.11

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const github = require('@actions/github');
 const { createDeployment, getBearerToken } = require('./api');
 
 const formattedRef = () => {
-  const { ref } = github.context;
+  const { ref, event_name, head_ref } = github.context;
+
+  if (event_name === 'pull_request') {
+    return head_ref;
+  }
 
   if (ref.startsWith('refs/heads/')) {
     return ref.replace('refs/heads/', '');
@@ -21,7 +25,6 @@ const main = async () => {
     const ref = formattedRef();
     core.debug(`Bearer token: ${token}`);
     core.debug(`Ref: ${ref}`);
-    core.debug('Context: ', JSON.stringify(github.context));
 
     await createDeployment(token, formattedRef());
   } catch (err) {
